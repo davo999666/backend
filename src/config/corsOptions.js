350c7ef915a52abfe4ff8d3fd672dca2f4892466
David1991Grig <davoaleks1991@gmail.com>
2025-09-05 10:41:18 +0300

    first

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,7 +1,7 @@
 export const corsOptions = {
-    origin: process.env.CORS_ORIGIN || "*", // your React frontend URL
+    origin: process.env.CORS_ORIGIN || "*", // frontend URL or all origins
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Password'],
-    credentials: true, // allow cookies if backend uses them
-    maxAge: 3600,
+    credentials: true, // allow cookies
+    maxAge: 3600, // seconds
 };
